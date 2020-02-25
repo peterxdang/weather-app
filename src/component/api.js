60523676192bd019350f7d9a1659b927c643201c
Peter Dang <Peter.x.Dang@gmail.com>
2020-02-24 20:52:56 -0600

    create component directory and components for rest API

---
+++ src/component/api.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class Api extends Component {
+
+render () {
+    return (
+        <div>
+           
+        </div>
+
+
+    )
+}
+}
+
+export default Api;
